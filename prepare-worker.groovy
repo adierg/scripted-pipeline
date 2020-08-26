8b253097e52cd77f607748667da9aa41be34e810
adinaErg <aidinaika@gmail.com>
2020-08-26 00:44:01 +0000

    ssh jenkins

---
+++ prepare-worker.groovy
@@ -0,0 +1,13 @@
+node {
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        stage('Init') {
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@104.131.115.88 yum install epel-release -y'
+        }
+        stage("Install git") {
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@104.131.115.88 yum install git -y'
+        }
+        stage("Install Java"){
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@104.131.115.88 yum install java-1.8.0-openjdk-devel -y'
+        }
+    }
+}
